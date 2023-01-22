6b59620a4ec2e410d191c747de56e1db940c0262
Abu Osama <112826965+Abu1Osama@users.noreply.github.com>
2023-01-23 00:00:27 +0530

    okk

--- yusi-sam/src/App.js
+++ yusi-sam/src/App.js
@@ -1,20 +1,16 @@
 import './App.css';
-// import Navbar from './Component/Navbar';
 import Footer from './Component/Footer';
 import Allroutes from './Component/Allroutes';
 import { BrowserRouter } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css"
-// import Navbarfinal from './Component/Navbarfinal';
 import { Nav } from './Component/Nav';
 
 function App() {
   return (
     <>
         <BrowserRouter>
-     {/* <Navbarfinal/> */}
         
-{/* <Navbar/> */}
 
      <Nav/>
 
